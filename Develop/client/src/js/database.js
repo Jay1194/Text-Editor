4b7a03f6c8e24eed632c49bc220c233804b4dd45
Jay <j.thomastt1@gmail.com>
2022-11-16 12:27:54 -0600

    added Get function

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -23,6 +23,16 @@ export const putDb = async (content) => {
 };
 
 //Getdb
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate','readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+   result
+   ? console.log('Found content')
+   : console.log('Content not found.');
+  return result?.value;
+};
 
 initdb();
