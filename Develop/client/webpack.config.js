33d163fb67362df3f5d524e90c72f448e728cf63
Jay <j.thomastt1@gmail.com>
2022-11-16 16:37:00 -0600

    deleted random files accidently downloaded in gitlab

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE'
       }),
-      new InjectManifest({
+       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
