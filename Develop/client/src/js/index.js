17fd89faac59933b424382aa772bf4232cca8d58
Jayden Taylor <j.thomastt1@gmail.com>
2024-06-05 12:19:17 -0700

    offline functionality

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -3,6 +3,7 @@ import Editor from './editor';
 import './database';
 import '../css/style.css';
 import Logo from '../images/logo.png';
+import './database';
 
 window.addEventListener('load', function () {
   document.getElementById('logo').src = Logo;
@@ -31,7 +32,7 @@ if (typeof editor === 'undefined') {
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox('/service-worker.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -21,7 +21,11 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
-      })
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
     ],
     module: {
       rules: [
